8daa7071c84bc1cc380803f8d4b53f80ee64f65f
AndersonRMac <anderson.ramosmaciel@gmail.com>
2024-07-03 15:59:10 -0300

    ajustes do angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule} from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CardContainerComponent } from './components/card-container/card-container.component';
+import { MatHeaderComponent } from './components/mat-header/mat-header.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardContainerComponent,
+    MatHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatTabsModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
---
+++ src/app/components/mat-header/mat-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-mat-header',
+  templateUrl: './mat-header.component.html',
+  styleUrl: './mat-header.component.scss',
+})
+export class MatHeaderComponent {
+
+}
